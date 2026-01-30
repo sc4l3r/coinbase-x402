a03be265fb2f29ab8daaece1b19046a106d85264
scaler <240590331+sc4l3r@users.noreply.github.com>
2026-01-30 16:45:53 +0100

    feat(typescript): add keeta mechanism implementation

---
+++ typescript/packages/mechanisms/keeta/src/constants.ts
@@ -0,0 +1,21 @@
+/**
+ * KTA token addresses
+ */
+export const KTA_MAINNET_ADDRESS =
+  "keeta_anqdilpazdekdu4acw65fj7smltcp26wbrildkqtszqvverljpwpezmd44ssg";
+export const KTA_TESTNET_ADDRESS =
+  "keeta_anyiff4v34alvumupagmdyosydeq24lc4def5mrpmmyhx3j6vj2uucckeqn52";
+
+/**
+ * USDC token addresses
+ */
+export const USDC_MAINNET_ADDRESS =
+  "keeta_amnkge74xitii5dsobstldatv3irmyimujfjotftx7plaaaseam4bntb7wnna";
+export const USDC_TESTNET_ADDRESS =
+  "keeta_apna75yhhvnv4ei7ape55hndk4yepno7a7i2mhtiwahiygixjcnmvswxhnmnk";
+
+/**
+ * CAIP-2 network identifiers for Keeta
+ */
+export const KEETA_MAINNET_CAIP2 = "keeta:21378";
+export const KEETA_TESTNET_CAIP2 = "keeta:1413829460";
